ce30ceab72a2d64e77ca41e084ee56b248bfa926
kalmykov1981 <kalmikovw13@gmail.com>
2019-06-30 10:51:19 -0500

    Initial Commit

---
+++ Lesson3QuzletWebinar3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson3QuzletWebinar3
+//
+//  Created by Сергей Калмыков on 6/30/19.
+//  Copyright © 2019 Сергей Калмыков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
