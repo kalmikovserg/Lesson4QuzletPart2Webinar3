257d931433fe3dd174dec436e24cc1a64341904a
kalmykov1981 <kalmikovw13@gmail.com>
2019-07-07 12:56:53 -0500

    v2.1

--- Lesson3QuzletWebinar3/Controller/ResultViewController.swift
+++ Lesson3QuzletWebinar3/Controller/ResultViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import WebKit
 
 class ResultViewController: UIViewController {
 
@@ -14,10 +15,43 @@ class ResultViewController: UIViewController {
    
     @IBOutlet var resaltLabel: UILabel!
     
+    @IBOutlet var webPage: WKWebView!
+    
+    @IBOutlet var labelFate: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updareUI()
         navigationItem.hidesBackButton = true
+        labelFate.isHidden = false
+    }
+    
+    @IBAction func burgerAction(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func writeAction(_ sender: UIButton) {
+        var statusView = false
+        let siteViewArray = view.subviews
+        let siteView = siteViewArray
+        for line in siteView {
+            if line.tag == 13 {
+                for interval in stride(from: 0, through: 0.8, by: 0.001) {
+                   line.alpha = CGFloat(interval)
+                    statusView = true
+                labelFate.isHidden = true
+                }
+            }
+        }
+        if statusView {
+            showFate()
+        }
+    }
+    
+    private func showFate() {
+      let url = "https://8-gund.com/ru/"
+      guard let urlRight = URL(string: url) else { return }
+      let urlRequest = URLRequest(url: urlRight)
+      webPage.load(urlRequest)
     }
     
     private func updareUI() {
